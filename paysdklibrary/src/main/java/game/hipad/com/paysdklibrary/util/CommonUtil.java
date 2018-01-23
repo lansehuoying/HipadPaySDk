06f710fc13ae98304c507a797bf738f807c978c7
tianjunjie <769168121@qq.com>
2018-01-23 11:46:55 +0800

    添加工具类支付前判断是否安装叮咚

---
+++ paysdklibrary/src/main/java/game/hipad/com/paysdklibrary/util/CommonUtil.java
@@ -0,0 +1,33 @@
+package game.hipad.com.paysdklibrary.util;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tianjunjie on 2018/1/23.
+ */
+
+public class CommonUtil {
+    //判断App是否安装
+    public static  boolean isAppInstalled(Context context) {
+       String packageName="com.jkrm.haipai.paixin.tc";
+        final PackageManager packageManager = context.getPackageManager();
+        List<PackageInfo> pinfo = packageManager.getInstalledPackages(0);
+        List<String> pName = new ArrayList<String>();
+        if (pinfo != null) {
+            for (int i = 0; i < pinfo.size(); i++) {
+                String pn = pinfo.get(i).packageName;
+                pName.add(pn);
+            }
+        }
+        if(!pName.contains(packageName)){
+            Toast.makeText(context,"您没有安装叮咚",Toast.LENGTH_SHORT).show();
+        }
+        return pName.contains(packageName);
+    }
+}
